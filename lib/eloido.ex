8aafc2c229b5efbcefb659d384af520c3fa64c8d
niku <niku@niku.name>
2014-08-14 21:20:03 +0900

    Scaffold

--- lib/eloido.ex
+++ lib/eloido.ex
@@ -1,2 +1,22 @@
 defmodule Eloido do
+  @credential_key "TWITTER_AUTH"
+
+  def credential_values, do: System.get_env(@credential_key) |> String.split(":")
+
+  def twitter_credential do
+    ~w(
+       consumer_key
+       consumer_secret
+       access_token
+       access_token_secret
+    )a |> Enum.zip(credential_values)
+  end
+
+  def start do
+    ExTwitter.configure(twitter_credential)
+    stream = ExTwitter.stream_filter(track: "apple") |>
+      Stream.map(fn(x) -> x.text end) |>
+      Stream.map(fn(x) -> IO.puts "#{x}\n---------------\n" end)
+    Enum.to_list(stream)
+  end
 end
