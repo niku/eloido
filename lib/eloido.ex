0ca5573b23ec636eb12c58aebed7a81f26e2107a
niku <niku@niku.name>
2014-08-18 13:34:14 +0900

    Set tracking value from environment variable

--- lib/eloido.ex
+++ lib/eloido.ex
@@ -1,7 +1,9 @@
 defmodule Eloido do
   @credential_key "TWITTER_AUTH"
+  @tracking_key "TRACK"
 
   def credential_values, do: System.get_env(@credential_key) |> String.split(":")
+  def tracking_values, do: System.get_env(@tracking_key)
 
   def twitter_credential do
     ~w(
@@ -14,7 +16,7 @@ defmodule Eloido do
 
   def start do
     ExTwitter.configure(twitter_credential)
-    stream = ExTwitter.stream_filter(track: "apple") |>
+    stream = ExTwitter.stream_filter(track: tracking_values) |>
       Stream.map(fn(x) -> x.text end) |>
       Stream.map(fn(x) -> IO.puts "#{x}\n---------------\n" end)
     Enum.to_list(stream)
