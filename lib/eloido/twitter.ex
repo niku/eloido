1c9a63ea70858153c7429d56fc438624178ebc36
niku <niku@niku.name>
2016-01-14 21:00:38 +0900

    Update Eloido.Twitter

--- lib/eloido/twitter.ex
+++ lib/eloido/twitter.ex
@@ -1,87 +1,30 @@
 defmodule Eloido.Twitter do
+
   require Logger
-
-  @hooking_value_matcher ~r/^(?<url>.+?)(?:@(?<user_ids>[0-9,]+))?(?:#(?<query>.+))?$/
-
-  def build_stream_parameter("", ""), do: build_stream_parameter([])
-  def build_stream_parameter(tracking_values, ""), do: build_stream_parameter(track: tracking_values)
-  def build_stream_parameter("", following_values), do: build_stream_parameter(follow: following_values)
-  def build_stream_parameter(tracking_values, following_values), do: build_stream_parameter(track: tracking_values, follow: following_values)
-  defp build_stream_parameter(parameter) do
-    Logger.info("Params for statuses/filter: #{inspect parameter}")
-    if Enum.empty?(parameter) do
-      Logger.warn("At least one of environment variables TRACK or FOLLOW must be setted")
-    end
-    parameter
-  end
-
-  def hook_configurations(hooking_values) do
-    configurations = Enum.map(hooking_values,
-                              &(Regex.named_captures(@hooking_value_matcher, elem(&1, 1))))
-    Logger.info("Hook Configurations: #{inspect configurations}")
-    configurations
-  end
-
-  def notify(url, message) do
-    case HTTPoison.post(url,
-                        URI.encode_query(%{source: message, format: "html"}),
-                        [{"Content-Type", "application/x-www-form-urlencoded"}]) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
-        Logger.debug(~s(Notify succeed url: #{url}, message: #{message}))
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        Logger.debug(~s(Notify failure url: #{url}, message: #{message}, reason: #{reason}))
-    end
-  end
 
   def start do
     twitter_credential = Application.fetch_env!(:eloido, :twitter)
-    tracking_values = Application.fetch_env!(:eloido, :track) || ""
-    following_values = Application.fetch_env!(:eloido, :follow) || ""
-    hooking_values = Application.fetch_env!(:eloido, :hook)
+    track = Application.fetch_env!(:eloido, :track) || ""
+    follow = Application.fetch_env!(:eloido, :follow) || ""
+    hook = Application.fetch_env!(:eloido, :hook)
 
     ExTwitter.configure(twitter_credential)
-    stream_parameter = build_stream_parameter(tracking_values, following_values)
-    twitter_stream = ExTwitter.stream_filter(stream_parameter, :infinity)
+    twitter_stream = Eloido.Twitter.StreamingParameter.build(track, follow) |> ExTwitter.stream_filter
+    hooks = Enum.map(hook, &Eloido.Twitter.Hook.parse/1)
 
-    hooks = hook_configurations(hooking_values)
+    do_streaming(twitter_stream, hooks)
+  end
 
-    for hook <- hooks,
-    tweet <- twitter_stream,
-    HookMatcher.match?(hook, tweet) do
-      if retweet?(tweet) do
-        Logger.debug("Skip retweet")
-      else
-        notify(hook["url"], build_message(tweet))
-      end
+  defp do_streaming(twitter_stream, hooks) do
+    for tweet <- twitter_stream,
+    hook <- hooks,
+    Hook.match_tweet?(hook, tweet),
+    !Tweet.retweet?(tweet) do
+      content = Eloido.Idobata.build_content(tweet)
+      idobata_custom_hook = %Eloido.Idobata.Hook{endpoint: hook.url, source: content, format: :html}
+      Task.start(Eloido.Idobata, :post, [idobata_custom_hook])
     end
-  end
 
-  defp retweet?(tweet) do
-    # tweet.retweeted remains always false.
-    # tweet.retweet_count remains always 0.
-    # So, we use tweet.text for checking retweet.
-    Logger.debug(~s(Checking text: #{tweet.text}, retweet?: #{String.starts_with?(tweet.text, "RT @")}))
-    String.starts_with?(tweet.text, "RT @")
-  end
-
-  defp build_message(tweet) do
-    user = tweet.user
-    url_user = "https://twitter.com/#{user.screen_name}"
-    url_tweet = "https://twitter.com/#{user.screen_name}/status/#{tweet.id_str}"
-    EEx.eval_file("lib/notify.eex",
-                  user_name: user.name,
-                  user_screen_name: user.screen_name,
-                  user_profile_image_url_https: user.profile_image_url_https,
-                  tweet_created_at: format_time(tweet.created_at),
-                  tweet_text: tweet.text,
-                  url_user: url_user,
-                  url_tweet: url_tweet)
-  end
-
-  defp format_time(twitter_time) do
-    {:ok, datetime} = Timex.DateFormat.parse(twitter_time, "{UNIX}")
-    datetime
-    |> Timex.Timezone.convert(Timex.Timezone.get("Asia/Tokyo"))
-    |> Timex.DateFormat.format!("%Y-%m-%d %T", :strftime)
+    do_streaming(twitter_stream, hooks)
   end
 end
