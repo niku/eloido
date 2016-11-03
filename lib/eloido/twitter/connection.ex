4b375493fabebefe7b2d476ad5d2d2c4a58c0c9c
niku <niku@niku.name>
2016-11-03 20:37:02 +0900

    Move function
    Eloido.Idobata.post/2 ->
    Eloido.Twitter.Connection.post/2

--- lib/eloido/twitter/connection.ex
+++ lib/eloido/twitter/connection.ex
@@ -20,8 +20,19 @@ defmodule Eloido.Twitter.Connection do
   end
 
   def handle_info({url, tweet = %ExTwitter.Model.Tweet{}}, state) do
-    Task.start(Eloido.Idobata, :post, [url, tweet])
+    Task.start(Eloido.Twitter.Connection, :post, [url, tweet])
     {:noreply, state}
+  end
+
+  def post(endpoint, tweet = %ExTwitter.Model.Tweet{}) do
+    body = URI.encode_query(source: Eloido.Twitter.Builder.build_source(tweet), format: :html)
+    headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
+    case HTTPoison.post(endpoint, body, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
+        Logger.debug(~s(Notify succeed endpoint: #{endpoint}, text: #{tweet.text}))
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.debug(~s(Notify failure endpoint: #{endpoint}, text: #{tweet.text}, reason: #{reason}))
+    end
   end
 
   defp do_start_stream(%Config{twitter: twitter, hooks: hooks}) do
