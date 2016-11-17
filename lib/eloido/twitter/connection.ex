002bb9d382a941dd0a52014818fb2dffd8fc8699
niku <niku@niku.name>
2016-11-17 19:35:05 +0900

    Extract to a module `Eloido.Twitter.Tweet`

--- lib/eloido/twitter/connection.ex
+++ lib/eloido/twitter/connection.ex
@@ -12,31 +12,14 @@ defmodule Eloido.Twitter.Connection do
     {:ok, {config, stream}}
   end
 
-  def handle_info({url, tweet = %ExTwitter.Model.Tweet{}}, state) do
-    Task.start(Eloido.Twitter.Connection, :post, [url, tweet])
-    {:noreply, state}
-  end
-
-  def post(endpoint, tweet = %ExTwitter.Model.Tweet{}) do
-    body = URI.encode_query(source: Eloido.Twitter.Builder.build_source(tweet), format: :html)
-    headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
-    case HTTPoison.post(endpoint, body, headers) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
-        Logger.debug(~s(Notify succeed endpoint: #{endpoint}, text: #{tweet.text}))
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        Logger.debug(~s(Notify failure endpoint: #{endpoint}, text: #{tweet.text}, reason: #{reason}))
-    end
-  end
-
   defp do_start_stream(%{oauth_token: oauth_token, streaming_parameter: streaming_parameter, hooks: hooks}) do
-    me = self
     hooks = Enum.map(hooks, &Eloido.Twitter.Hook.parse/1)
     Task.start_link(fn ->
       for tweet <- Eloido.Twitter.Stream.produce(oauth_token, streaming_parameter),
           is_nil(tweet.retweeted_status),
           hook <- hooks,
           Eloido.Twitter.Hook.match_tweet?(hook, tweet) do
-        send(me, {hook.url, tweet})
+        Task.start(Eloido.Twitter.Tweet, :post, [hook.url, tweet])
       end
     end)
   end
